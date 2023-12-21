4a85ed6e1467d08d36c2d4f7879d02631db007db
Saurabh Paryani <saurabhparyani64@gmail.com>
2023-12-22 01:18:53 +0530

    Added solutions to middleware problems

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -13,15 +13,35 @@ const app = express();
 
 let numberOfRequestsForUser = {};
 setInterval(() => {
-    numberOfRequestsForUser = {};
+  numberOfRequestsForUser = {};
 }, 1000)
 
-app.get('/user', function(req, res) {
+app.use(function (req, res, next) {
+  const userId = req.headers["user-id"];
+  if (numberOfRequestsForUser[userId]) {
+    numberOfRequestsForUser[userId]++;
+    if (numberOfRequestsForUser[userId] > 5) {
+      res.status(404).send("No entry")
+    }
+    else {
+      next();
+    }
+  }
+  else {
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
+})
+
+
+app.get('/user', function (req, res) {
   res.status(200).json({ name: 'john' });
 });
 
-app.post('/user', function(req, res) {
+app.post('/user', function (req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
+
+app.listen(3000);
 
 module.exports = app;
